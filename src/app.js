55d791191a87abcac9cc427e8a98fc62bec1872a
itsmeganeshadhikari@gmail.com <itsmeganeshadhikari@gmail.com>
2023-10-11 17:21:58 +0545

    CodeIt -"Connection to mongodb atlast"

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require("express");   // creating express server
 const userRoute = require("./routes/user.route");
-const { createConnection } = require("./db/connection");
+const db_connect = require("./db/connection");
 const dotenv = require("dotenv").config();   // to include env file in this code
 
 const app = express();
 app.use(express.json())
-const conn =  createConnection();
+const conn = db_connect();
 console.log(conn);
 app.use("/api",userRoute);
 app.get("/", (req, res) => {
--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,20 @@
-const { default: mongoose } = require("mongoose");
-
-exports.createConnection = async() =>{
-    const connection =  await mongoose.createConnection('mongodb://localhost:27017/ecommerce') //localhost connection method
-    return connection;
+const { default: mongoose } = require("mongoose")
+const dotenv =  require('dotenv').config()
+var debug = require('debug')('http')
+const db_connect = async() => {
+  try {
+    const conn = await mongoose.connect(encodeURI(process.env.CONNECTION), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    console.log(conn);
+    return conn;
+  } catch (error) {
+    debug('error')
+    console.log('error');
+  }
+  return ''
 }
+
+module.exports = db_connect;
+
