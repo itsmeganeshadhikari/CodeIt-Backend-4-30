54cf8be025f4c98713e0af4755bb72103172a773
itsmeganeshadhikari@gmail.com <itsmeganeshadhikari@gmail.com>
2023-10-17 17:31:29 +0545

    CodeIt 'Array to single object"

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -4,11 +4,11 @@ const User = require("../models/Users")
 
 exports.validateRequest = async(req,res) =>{
     try {
-            const user = await User.find({email: req.body.email});
+            const user = await User.findOne({email: req.body.email});
             if(!user){
                 res.status(200).send("Email not found");
             }else{
-                const condition = await validatePassword(req.body.password, user[0].password);
+                const condition = await validatePassword(req.body.password, user.password);
                 if(condition){
                     login(req.body.email, req, res);
                 }
